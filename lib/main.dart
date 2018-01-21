2c051dc01a6ba3cc75664d1b3d6c80cf2ebbf36a
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-01-20 17:15:38 -0800

    Remove unnecessary demo app code.

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Floodo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new FloodoList(title: 'Floodo'),
@@ -48,16 +39,9 @@ class _FloodoListState extends State<FloodoList> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       body: new ListView(children: items.map(buildItem).toList(),),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
